976c630406f48cfadc40e71dedbaeb45832cc58d
Jakub Prokes <jprokes@redhat.com>
2014-11-20 14:15:29 +0100

    Add license and legal info.

--- test/tman-test.c
+++ test/tman-test.c
@@ -1,3 +1,26 @@
+/*
+ ******************************************************************************
+ * This file is part of Tman.
+ *
+ * Tman is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tman.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2014 Red Hat, Inc., Jakub Prokeš <jprokes@redhat.com>
+ * Author: Jakub Prokeš <jprokes@redhat.com>
+ *
+ ******************************************************************************
+ */
+
 #define _GNU_SOURCE
 
 #include <time.h>
